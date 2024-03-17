bfaaeaef0d66605fc9be03051345734d5c0074cc
VinamraSaurav <80186642+VinamraSaurav@users.noreply.github.com>
2024-03-17 12:40:16 +0530

    basic ui design

--- src/components/Fan.jsx
+++ src/components/Fan.jsx
@@ -16,18 +16,21 @@ const Fan = () => {
         }
     }
   return (
-    <div className='w-[200px] bg-white rounded-md p-4 flex flex-col items-center gap-3 bg-[white]'>
+    <div className='w-[200px] bg-white rounded-xl p-4 flex flex-col items-center gap-3 bg-[white] '>
         <div className='font-popins font-bold text-black text-xl text-center my-2'>Fan</div>
         <div className='font-popins text-sm text-black '>Control the speed of the fan</div>
         <div className='flex justify-center items-center w-[60px] h-[60px] m-3'>
             <Image src='/images/fan.png' height={100} width={100} className='h-100 w-100'/>
         </div>
         <div className='flex items-center justify-center gap-2'>
-            <button className='rounded-full p-2 px-4 bg-color-1' onClick={handleClickMinus}>-</button>
-            <div className='rounded-md px-5 py-1 bg-color-1 w-[80px] flex justify-center'>
-                {speed}
+        <button className={`rounded-full p-2 px-4 bg-color-1 ${speed===0?'opacity-50 cursor-not-allowed':'opacity:100 cursor-pointer'} `} onClick={handleClickMinus} disabled={speed===0?true:false}>-</button>
+            <div className='rounded-md px-5 py-1 bg-color-1 w-[80px]'>
+                <div>
+                    <div className='text-xs font-popins flex justify-start'>Speed:</div>
+                    <div className='text-sm font-popins flex justify-center'>{speed}</div>
+                </div>
             </div>
-            <button className='rounded-full p-2 px-4 bg-color-1' onClick={handleClickPlus}>+</button>
+            <button className={`rounded-full p-2 px-4 bg-color-1 ${speed===5?'opacity-50 cursor-not-allowed':'opacity:100 cursor-pointer'} `} onClick={handleClickPlus} disabled={speed===5?true:false}>+</button>
         </div>
             
     </div>
