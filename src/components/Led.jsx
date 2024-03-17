3b5de3a948b98b5c4162bdf4d68c1ad9ba164062
VinamraSaurav <80186642+VinamraSaurav@users.noreply.github.com>
2024-03-17 13:26:29 +0530

    led component

--- src/components/Led.jsx
+++ src/components/Led.jsx
@@ -7,27 +7,26 @@ const Led = () => {
 
     
   return (
-    <div className='w-[200px] bg-white rounded-xl p-4 flex flex-col items-center gap-3 bg-[white] '>
-        <div className='font-popins font-bold text-black text-xl text-center my-2'>Fan</div>
-        <div className='font-popins text-sm text-black '>Control the speed of the fan</div>
+    <div className='w-[240px] bg-white rounded-xl p-4 flex flex-col items-center gap-3 bg-[white] h-[360px]'>
+        <div className='font-popins font-bold text-black text-xl text-center my-2'>LED</div>
+        <div className='font-popins text-sm text-black h-[30px]'>Control the color of led</div>
         <div className='flex justify-center items-center w-[60px] h-[60px] m-3'>
             <Image src='/images/led.png' height={100} width={100} className='h-100 w-100'/>
         </div>
         <div className='flex items-center justify-center gap-2'>
         
-            <div className='rounded-md px-5 py-1 bg-color-1 w-[120px]'>
-                <div className='flex'>
+            <div className='rounded-md px-5 py-1 bg-color-1 w-[120px] mx-auto'>
+            <div className='flex flex-col justify-center items-center'>
                     <div className='text-xs font-popins flex justify-start'>Color:</div>
-                    <div className={`text-sm font-popins flex justify-center h-4 w-4 mx-2 rounded-full bg-[${color}]`}></div>
+                    <div className={`text-sm font-popins flex justify-center h-4 w-4 mx-2 rounded-full bg-[${color}]`}>{color}</div>
                 </div>
-                <div>
-                <div className='text-xs font-popins flex justify-start'>Choose Color:</div>
-                <div className='rounded-full'><input className='rounded-full' value={color} type='color' onChange={(e)=>{
+                {/* <button className={`rounded-full p-2 px-4 bg-color-1 ${speed===5?'opacity-50 cursor-not-allowed':'opacity:100 cursor-pointer'} `} onClick={handleClickPlus} disabled={speed===5?true:false}>+</button> */}
+                
+                
+            </div>
+            <div className='rounded-full p-2  bg-color-1 flex items-center justify-center'><input className='rounded-full h-6 w-6' value={color} type='color' onChange={(e)=>{
                     setColor(e.target.value);
                 }}></input></div>
-                
-                </div>
-            </div>
             
         </div>
             
--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Fan from './Fan'
 import AirConditioner from './AirCondtioner'
+import Led from './Led'
 
 const Tools = () => {
   return (
@@ -9,6 +10,7 @@ const Tools = () => {
         <div className='text-xl font-popins font-bold text-center my-4'>Our Services</div>
         <div className='flex flex-wrap justify-center items-center max-w-6xl mx-auto gap-4'>
             <Fan/>
+            <Led/>
             <AirConditioner/>
 
         </div>
